d7b0cba4af095b416d4e89e3b3d26383b2444070
Andy Tran <duy.tran@ampup.io>
2022-03-26 20:16:18 +0700

    update

--- deploy_github_pages.sh
+++ deploy_github_pages.sh
@@ -11,6 +11,3 @@ git add dist -f
 git commit -m "Adding dist"
 
 git subtree push --prefix dist origin gh-pages
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:rualucky/ndp-2.git main:gh-pages
