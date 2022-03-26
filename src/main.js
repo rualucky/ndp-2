7505c1f97705f4e043e2c40e2b7d76242ec10684
Andy Tran <duy.tran@ampup.io>
2022-03-26 20:34:08 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_AkvMmxPye_CV6S4T-XriYHj9CdnLXps",
+  authDomain: "ndp-2-f045f.firebaseapp.com",
+  projectId: "ndp-2-f045f",
+  storageBucket: "ndp-2-f045f.appspot.com",
+  messagingSenderId: "90380981956",
+  appId: "1:90380981956:web:0868bc9ac723f098165b91",
+  measurementId: "G-R6RQYKYLVD"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createApp(App).mount('#app')
